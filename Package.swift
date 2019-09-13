8d49adf9357cdbd83383fd76c861f23f17c7c05e
brandon.levasseur <brandon.levasseur@universalorlando.com>
2019-09-13 10:59:07 -0400

    - Added SPM Package file to project - Adjusted folder structure for SPM
    Signed-off-by: brandon.levasseur <brandon.levasseur@universalorlando.com>

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.1
+import PackageDescription
+
+let package = Package(
+    name: "FastImageCache",
+    platforms: [
+        .iOS(.v11)
+    ],
+    products: [
+        .library(name: "FastImageCache", targets: ["FastImageCache"]),
+    ],
+    targets: [
+        .target(name: "FastImageCache"),
+        .testTarget(name: "FastImageCacheTests", dependencies: ["FastImageCache"]),
+    ]
+)
