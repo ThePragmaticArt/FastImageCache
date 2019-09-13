d7ec2b9777786b568d63c7ed392b50ced8a34bb7
brandon.levasseur <brandon.levasseur@universalorlando.com>
2019-09-13 12:36:25 -0400

    Removed dynamic framework flag
    Signed-off-by: brandon.levasseur <brandon.levasseur@universalorlando.com>

--- Package.swift
+++ Package.swift
@@ -8,7 +8,6 @@ let package = Package(
     ],
     products: [
         .library(name: "FastImageCache",
-                 type: .dynamic,
                  targets: ["FastImageCache"])
     ],
     targets: [
